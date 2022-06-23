61da7074628280f229c00bdedfde6a7e9c1d89c9
Vladoslav Kozenok <v.kozenok@hsdigital.ru>
2022-06-23 17:52:43 +0700

    .

--- internal/fsm/model.go
+++ internal/fsm/model.go
@@ -1,14 +1,5 @@
 package fsm
 
-import (
-	"Gives_SDT_Bot/pkg/logging"
-)
-
-type FSM struct {
-	repository Repository
-	logger     *logging.Logger
-}
-
 type UserState struct {
 	tableName struct{} `pg:"public.users_states"`
 	UserID    int      `pg:"user_id,fk,unique"`
